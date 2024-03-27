4d2330b715aeb52c475e5581f7f4e29409c6120a
YaganArun <yagan.arun@yahoo.com>
2024-03-27 18:20:05 +0000

    react app init & tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex justify-center mt-8">
+        <h1 className="app-title text-2xl font-thin">netflix clone app 🚀</h1>
+      </div>
     </div>
   );
 }
