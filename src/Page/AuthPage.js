6536bd59823ff8b9cd319ae1f9909bf2e2027d92
YaganArun <yagan.arun@yahoo.com>
2024-03-28 21:16:31 +0000

    added form validation

--- src/Page/AuthPage.js
+++ src/Page/AuthPage.js
@@ -6,7 +6,7 @@ export default function AuthPage() {
       <div className="login-form absolute z-20 my-auto top-0 bottom-0 mx-auto left-0 right-0 w-[30%] h-[60%]">
         <LoginForm></LoginForm>
       </div>
-      <div className="bg-black absolute z-10  my-auto top-0 bottom-0 mx-auto left-0 right-0 w-[30%] h-[60%] opacity-70 rounded-lg"></div>
+      <div className="bg-black absolute z-10  my-auto top-0 bottom-0 mx-auto left-0 right-0 w-[30%] h-[80%] opacity-70 rounded-lg"></div>
       <div className="bg-black">
         <div
           className="opacity-0 sm:opacity-0 h-screen w-screen md:opacity-50"
--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,9 +1,52 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 export default function LoginForm() {
   const [isSignIn, setIsSignIn] = useState(true);
+  const [isValidName, setIsValidName] = useState(true);
+  const [isValidEmail, setIsValidEmail] = useState(true);
+  const [isValidPassword, setIsValidPassword] = useState(true);
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
 
   function handleSignUp() {
     setIsSignIn(!isSignIn);
+    setIsValidEmail(true);
+    setIsValidPassword(true);
+  }
+
+  function validateName() {
+    const isValid =
+      /(^[A-Za-z]{3,16})([ ]{0,1})([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})/.test(
+        name.current.value
+      );
+    if (!isValid) {
+      setIsValidName(false);
+    } else {
+      setIsValidName(true);
+    }
+  }
+
+  function validateEmail() {
+    const isValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(
+      email.current.value
+    );
+    if (!isValid) {
+      setIsValidEmail(false);
+    } else {
+      setIsValidEmail(true);
+    }
+  }
+
+  function validatePassword() {
+    const isValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(
+      password.current.value
+    );
+    if (!isValid) {
+      setIsValidPassword(false);
+    } else {
+      setIsValidPassword(true);
+    }
   }
 
   return (
@@ -13,22 +56,47 @@ export default function LoginForm() {
       </h1>
       <form className="flex flex-col">
         {!isSignIn && (
-          <input
-            type="text"
-            placeholder="Full Name"
-            className="p-2 m-2 bg-transparent"
-          ></input>
+          <div className="flex flex-col flex-1">
+            <input
+              ref={name}
+              type="text"
+              placeholder="Full Name"
+              className="p-2 m-2 bg-transparent text-white border border-white rounded-lg "
+              onBlur={validateName}
+            ></input>
+            {!isValidName && (
+              <div className="flex justify-start m-2">
+                <p className=" text-red-700">Please enter a valid full name</p>
+              </div>
+            )}
+          </div>
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email Address"
-          className="p-2 m-2 bg-transparent"
+          className="p-2 m-2 bg-transparent text-white border border-white rounded-lg"
+          onBlur={validateEmail}
         ></input>
+        {!isValidEmail && (
+          <div className="flex justify-start m-2">
+            <p className=" text-red-700">Please enter a valid email address</p>
+          </div>
+        )}
         <input
+          ref={password}
           type="password"
           placeholder="Password"
-          className="p-2 m-2 bg-transparent"
+          className="p-2 m-2 bg-transparent text-white border border-white rounded-lg"
+          onBlur={validatePassword}
         ></input>
+        {!isValidPassword && (
+          <div className="flex justify-start m-2">
+            <p className=" text-red-700">
+              Your password must contain between 4 and 60 characters.
+            </p>
+          </div>
+        )}
         <button
           type="submit"
           className="p-4 m-4 bg-red-600 text-white rounded-xl"
